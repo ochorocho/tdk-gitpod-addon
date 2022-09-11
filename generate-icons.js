8f76864b1aece108b7d95321dc9f40e7192b4388
ochorocho <jochen.roth@b13.com>
2022-09-11 13:53:15 +0200

    [TASK] Generate DMG file

--- generate-icons.js
+++ generate-icons.js
@@ -5,6 +5,7 @@ const glob = require('glob')
 
 const iconsFolder = path.join(__dirname, 'icons/')
 const iconsTargetFolder = path.join(__dirname, './dist/icons/')
+const iconsTargetFolderSafari = path.join(__dirname, './dist-safari/icons/')
 
 // Create target folder if it does not exist
 const dir = path.join(iconsTargetFolder)
@@ -12,15 +13,22 @@ if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir, 0o744)
 }
 
+// Safari - create target folder if it does not exist
+const dirSafari = path.join(iconsTargetFolderSafari)
+if (!fs.existsSync(dirSafari)) {
+  fs.mkdirSync(dirSafari, 0o744)
+}
+
 // Get files to process
 glob(path.join(iconsFolder, '**/*.svg'), function (er, files) {
   files.forEach((file) => {
-    renderIcon(file)
+    renderIcon(file, iconsTargetFolder)
+    renderIcon(file, iconsTargetFolderSafari, [48, 512])
   })
 })
 
 // Generate icon sizes
-function renderIcon(file, sizes = [48, 96, 128]) {
+function renderIcon(file, targetFolder, sizes = [48, 96, 128]) {
   const filename = path.parse(path.basename(file))
 
   sizes.forEach((size) => {
@@ -28,7 +36,7 @@ function renderIcon(file, sizes = [48, 96, 128]) {
       .rotate()
       .resize(size)
       .png()
-      .toFile(path.join(iconsTargetFolder, filename.name + '-' + size + '.png'))
+      .toFile(path.join(targetFolder, filename.name + '-' + size + '.png'))
       .catch(err => {
         console.log('Could not write file ...', err)
       })
